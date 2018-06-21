7fe5e4c351bbf14762305dd888c846d5652078d8
zaccc <i@zaccc.tk>
2018-06-21 13:46:06 +0800

    update README.md

---
+++ README.md
@@ -0,0 +1,28 @@
+# jwzxSpider
+一个通用版的教务在线信息爬虫
+
+## Based on
+
+#### Environment
+
+- Python3.6 or later
+- mysql5.7
+
+#### pip package
+
+- requests
+- html-table-extractor
+- beautifulsoup4
+- mysql-connector-python
+
+## To run
+#### If local mysql
+`python main.py [username] [password] [database]`
+
+#### If mysql server
+
+`python main.py [username] [password] [database] [server]`
+
+## To do
+
+- faster crawl
---
+++ sqltool.py
@@ -0,0 +1,36 @@
+from mysql import connector
+
+
+class sql_con():
+    def __init__(self, user, password, database, host='127.0.0.1'):
+        try:
+            self.conn = connector.connect(
+                user=user, password=password, database=database, host=host)
+        except ConnectionError as e:
+            print(e)
+
+    def create_table(self):
+        self.cursor = self.conn.cursor()
+        sql = """CREATE TABLE `stu` (
+                  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                  `studentnum` int(30) DEFAULT NULL,
+                  `name` varchar(30) DEFAULT NULL,
+                  `gender` varchar(10) DEFAULT NULL,
+                  `classnum` int(30) DEFAULT NULL,
+                  `major` varchar(40) DEFAULT NULL,
+                  `college` varchar(60) DEFAULT NULL,
+                  PRIMARY KEY (`id`)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8;"""
+        self.cursor.execute(sql)
+        self.conn.commit()
+        self.cursor.close()
+
+    def set_info(self, data):
+        self.cursor = self.conn.cursor()
+        sql = f"""INSERT INTO stu 
+        ( studentnum, name, gender, classnum, major, college )
+        VALUES
+        ( {data[0]}, \"{data[1]}\", \"{data[2]}\", {data[3]}, \"{data[4]}\", \"{data[5]}\" );"""
+        self.cursor.execute(sql)
+        self.conn.commit()
+        self.cursor.close()
